250a6824c60744533443bf3fc4117eb73a6c1059
abdulla-li <abdullashakkeeb693@gmail.com>
2026-01-08 17:26:34 +0000

    udfps: enable MTK GHBM dimlayer support
    Enable MediaTek-specific UDFPS GHBM implementation and define the
    required dimlayer name to match recent frameworks changes.
    
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>

--- device.mk
+++ device.mk
@@ -338,8 +338,8 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.nothing
 
     # UDFPS dim layer call
-$(call soong_config_set,surfaceflinger,has_mtk_udfps,true)
-
+$(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
+$(call soong_config_set,surfaceflinger,mtk_dim_layer,NTFingerprintDimLayer)
 
 # Properties
 include $(LOCAL_PATH)/vendor_props.mk
