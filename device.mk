c4b1a469c2404a518a50a55c1472314fde316da8
abdulla-li <abdullashakkeeb693@gmail.com>
2026-01-09 19:08:48 +0530

    tetris: add saturation prop

--- device.mk
+++ device.mk
@@ -16,8 +16,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.activity_anim_perf_override=true \
     persist.sys.phh.mtk_ged_kpi=1 \
     ro.soc.manufacturer=MediaTek \
-    ro.soc.model=Dimensity_7300  
-    
+    ro.soc.model=Dimensity_7300 \
+    persist.sys.sf.color_saturation=0.759 
 
 # ART / JIT / Dex2Oat 
 PRODUCT_PRODUCT_PROPERTIES += \
