387f94be1f4a4fb3d5c70e3e8a06caed44ec49f8
abdulla-li <abdullashakkeeb693@gmail.com>
2026-04-03 13:58:34 +0000

    Tetris: Overlay: FrameworkResOverlayTetris => FrameworksResOverlayTetris

--- device.mk
+++ device.mk
@@ -141,7 +141,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    FrameworkResOverlayTetris \
+    FrameworksResOverlayTetris \
     LauncherOverlayTetris \
     SettingsResTetris \
     SystemUIOverlayTetris \
