94b0af9f0756ee9c007941628939723c9c85bc8d
abdulla-li <abdullashakkeeb693@gmail.com>
2026-04-20 10:36:47 +0000

    Bump to B4.0-260225-1904

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,5 +10,5 @@ if [ ! -d "vendor/nothing/Tetris" ]; then
   git clone https://gitlab.com/abdullashakkeeb693/tetris_vendor_tree -b a15 vendor/nothing/Tetris
 fi
 if [ ! -d "device/nothing/Tetris-kernel" ]; then
-  git clone https://github.com/abdulla-li/android_device_nothing_Tetris-kernel -b v3.2 device/nothing/Tetris-kernel
+  git clone https://github.com/abdulla-li/android_device_nothing_Tetris-kernel -b a16 device/nothing/Tetris-kernel
 fi
